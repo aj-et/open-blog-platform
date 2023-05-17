186c443af9672cdfa47648c514365bf24a9568fe
Hyrum Paramore <par17057@byui.edu>
2023-05-17 08:40:43 -0600

    cleaned up code

--- src/App.js
+++ src/App.js
@@ -1,29 +1,7 @@
 import './App.css';
 import Login from './partials/login.js';
 import Register from './partials/register.js';
-// import Login from './partials/login.js';
 
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 function App() {
   return (
