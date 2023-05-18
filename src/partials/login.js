4115d2bd1dbe16580373a06d08057f42193bce7a
tumbokon.aaronjulius@outlook.com <tumbokon.aaronjulius@outlook.com>
2023-05-18 09:05:45 -0600

    Addded authentication capability

--- src/partials/login.js
+++ src/partials/login.js
@@ -1,38 +1,53 @@
-// Use this to call the function in App.js
-{/* <div className="login-form">
-    {<Login></Login>}
-</div> */}
-
 import React, { useState } from "react";
+import { auth } from "../utils/firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 export default function Login() {
-return (
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-<div className="modal-content animate">
-    <div onclick="document.getElementById('id01').style.display='none'" className="close" title="Close Form">&times;</div>
-    
-    <div className="modal-header-text">
-        <h2>Login</h2>
-    </div>
+    const login = async (e) => {
+        e.preventDefault();
+        // const auth = getAuth();
+        signInWithEmailAndPassword(auth, email, password).then ((userCredential) => {
+            // const user = userCredential.user;
+            console.log(userCredential);
+        }).catch((error) => {
+            console.log(error);
+        });
 
-    <div className="container">
-        <form action="" method="post" id="login">
-            <label for="uname">Username</label>
-            <input type="text" placeholder="Enter Username" name="uname" required/>
-            <label for="psw">Password</label>
-            <input type="password" placeholder="Enter Password" name="psw" required/>
-            <button type="submit">Login</button>
-            <label>
-            <input type="checkbox" checked="checked" name="remember"/> <span>Remember me</span>
-            </label>
-        </form>
+        // This will empy out the fields after clicking button
+        setEmail('');
+        setPassword('');
+    };
 
-        <div className="container">
-            <h2>Don't have an account?</h2>
-            <div className="reg"><a href="#">Register</a></div>
+    return (
+        <div className="modal-content animate">
+            {/* We need to use hooks on this one */}
+            {/* <div onClick={`${document.getElementById('id01').style.display='none'}`} className="close" title="Close Form">&times;</div> */}
+            
+            <div className="modal-header-text">
+                <h2>Login</h2>
+            </div>
+
+            <div className="container">
+                <form action="" method="post" id="login" onSubmit={login}>
+                    <label htmlFor="uname">Username</label>
+                    <input type="email" placeholder="Enter Username" name="uname" value={email} onChange={(e) => setEmail(e.target.value)} required/>
+                    <label htmlFor="psw">Password</label>
+                    <input type="password" placeholder="Enter Password" name="psw" value={password} onChange={(e) => setPassword(e.target.value)} required/>
+                    <button type="submit">Login</button>
+                    <label>
+                    {/* The checked needs to be replaced with either an onClick or onChange? */}
+                    {/* <input type="checkbox" checked="checked" name="remember"/> <span>Remember me</span> */}
+                    </label>
+                </form>
+
+                <div className="container">
+                    <h2>Don't have an account?</h2>
+                    <div className="reg"><a href="#">Register</a></div>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
-
-)
-}
+    );
+};
