2e9f91b0ea60f9a69f35d615ff7ba26ee0519a71
Hyrum Paramore <par17057@byui.edu>
2023-07-09 23:26:59 -0600

    Removed an un-needed nav section

--- src/partials/Profile.js
+++ src/partials/Profile.js
@@ -87,8 +87,8 @@ function Profile() {
     return(
       <div>
         <Header />
-      <Navbar />
-      <h1>Thank you for visiting us, Please log in or sign up to view your profile</h1>
+      {/* <Navbar /> */}
+      <h2>Please log in or sign up to view your profile.</h2>
       
     
       </div>
